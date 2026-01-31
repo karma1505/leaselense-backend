c416791a2bcf0087ce026ac15fca31f38f6259b7
Karmanya Singh <karmanyasingh8@gmail.com>
2026-02-01 01:03:32 +0530

    alllow for ngrok

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"], # Allow all for ngrok
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
